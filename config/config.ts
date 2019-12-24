1245d5be20987ca0d53012223c1d3d708ce30a58
lochuynh <lochuynh12@gmail.com>
2019-12-24 16:08:38 +0700

    add update now.json

--- config/config.ts
+++ config/config.ts
@@ -6,7 +6,7 @@ import themePluginConfig from './themePluginConfig';
 const { pwa } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 
-const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, ANT_DESIGN_TOKEN } = process.env;
+const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, ANT_DESIGN_TOKEN, MY_SECRET } = process.env;
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
 const plugins: IPlugin[] = [
   [
@@ -347,6 +347,7 @@ export default {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
       ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
       'process.env.ANT_DESIGN_TOKEN': ANT_DESIGN_TOKEN,
+      'process.env.MY_SECRET': MY_SECRET,
   },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
